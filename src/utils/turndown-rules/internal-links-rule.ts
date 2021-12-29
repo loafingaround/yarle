1cc1aa7fc78bf55ca649d7ee34c48fe299ede165
Thomas Orton <thorton@fastmail.com>
2021-12-29 11:29:29 +0000

    Deduplicate declaration of and reuse variable

--- src/utils/turndown-rules/internal-links-rule.ts
+++ src/utils/turndown-rules/internal-links-rule.ts
@@ -62,8 +62,7 @@ export const wikiStyleLinksRule = {
         const renderedObsidianDisplayName = omitObsidianLinksDisplayName ? '' : `|${displayName}`;
 
         if (value.startsWith('evernote://')) {
-            const fileName = normalizeTitle(token['text']);
-            const displayName = token['text'];
+            const fileName = normalizeTitle(displayName);
             const noteIdNameMap = RuntimePropertiesSingleton.getInstance();
             if (isTOC(noteIdNameMap.getCurrentNoteName())) {
                 noteIdNameMap.addItemToTOCMap({ url: value, title: fileName });
