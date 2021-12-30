cade4f79bfe1197611a3e8ec5f0c4efb93975f42
Thomas Orton <thorton@fastmail.com>
2021-12-30 15:36:57 +0000

    If given directory path, also look in directories one level deep
    If the configured enex source is a directory then also look for enex
    files in its sub-directories, where each sub-directory represents a
    notebook stack (but no deeper as Evernote notebook stacks cannot contain
    notebook stacks).

--- src/yarle.ts
+++ src/yarle.ts
@@ -1,5 +1,5 @@
 // tslint:disable:no-console
-import fs from 'fs';
+import fs, { Dirent } from 'fs';
 import { merge } from 'lodash';
 import * as path from 'path';
 import flow from 'xml-flow';
@@ -46,6 +46,11 @@ export const defaultYarleOptions: YarleOptions = {
     headingStyle: 'atx',
   },
 };
+
+interface EnexSource {
+  path: string
+  notebookStackName: string
+}
 
 export let yarleOptions: YarleOptions = { ...defaultYarleOptions };
 
@@ -134,31 +139,42 @@ export const parseStream = async (options: YarleOptions, enexSource: string): Pr
 };
 
 export const dropTheRope = async (options: YarleOptions): Promise<Array<string>> => {
-  let enexSources: string[];
+  let enexSources: EnexSource[];
   if (options.enexSources.length === 1 && options.enexSources[0].endsWith('.enex')) {
     loggerInfo(`Converting notes in file: ${options.enexSources}`);
-    enexSources = options.enexSources;
+    enexSources = options.enexSources.map(enexFile => ({
+      "path": enexFile,
+      "notebookStackName": ""
+    }));
   } else {
-    // TODO: also get enex files one level deep
-    // TODO: somehow include folder (notebook stack) name in enexSources
     // TODO: add info log here (as above for single file)?
-    const enexFiles = fs
-      .readdirSync(options.enexSources[0])
-      .filter((file: any) => {
-        return file.match(/.*\.enex/ig);
-      });
-
-    enexSources = enexFiles.map(enexFile => `${options.enexSources[0]}${path.sep}${enexFile}`);
+    const baseDir = options.enexSources[0];
+    const enexEntries = fs.readdirSync(baseDir, {"withFileTypes": true});
+    let isEnexFile = ((entry: Dirent) => entry.isFile() && entry.name.match(/.*\.enex/ig));
+    enexSources = enexEntries
+      .filter(isEnexFile)
+      .map(enexFile => ({
+        "path": `${baseDir}/${enexFile.name}`,
+        "notebookStackName": ""
+      }))
+      .concat(...enexEntries
+        .filter(entry => entry.isDirectory())
+        .map(dir => fs.readdirSync(`${baseDir}/${dir.name}`, {"withFileTypes": true})
+          .filter(isEnexFile)
+          .map(enexFile => ({
+            "path": `${baseDir}/${dir.name}/${enexFile.name}`,
+            "notebookStackName": dir.name
+      }))));
   }
 
   clearLogFile();
   setOptions(options);
   const outputNotebookFolders = [];
   for (const enex of enexSources) {
-    utils.setPaths(enex);
+    utils.setPaths(enex.path);
     const runtimeProps = RuntimePropertiesSingleton.getInstance();
-    runtimeProps.setCurrentNotebookName(utils.getNotebookName(enex));
-    await parseStream(options, enex);
+    runtimeProps.setCurrentNotebookName(utils.getNotebookName(enex.path));
+    await parseStream(options, enex.path);
     outputNotebookFolders.push(utils.getNotesPath());
   }
 
